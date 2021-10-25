3f39059218193fbe442dd01efdc473d92229db67
Luigi_Bross <cursosinformatica9102@gmail.com>
2021-10-25 10:55:08 -0300

    Cria nav-menu responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#menu')
+const menuToggle = document.querySelector('.navbar')
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    menuToggle.classList.toggle('nav-toggle');
+});
